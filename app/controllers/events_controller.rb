b3e196c0b77b632f7cb5b54ba398d5f048c5e8ca
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-03-12 14:16:32 +0900

    イベント・実績ページを作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,44 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:edit, :update, :destroy]
   def index
     @events = Event.all
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @event.update_attributes(event_params)
+      redirect_to events_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :genre, :place, :describe, :start_at, :end_at)
   end
 end
